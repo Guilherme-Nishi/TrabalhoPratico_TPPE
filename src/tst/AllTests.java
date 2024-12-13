a3f679709adba06373c547fa14908b2dfb38c684
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-12-13 16:10:57 -0300

    adiciona teste na suite

--- src/tst/AllTests.java
+++ src/tst/AllTests.java
@@ -13,7 +13,9 @@ import org.junit.runners.Suite.SuiteClasses;
 				TesteCadastroContribuicaoPrevidenciaria.class, 
 				TesteCadastroPensaoAlimenticia.class, 
 				TesteCadastroOutrasDeducoes.class,
-				TesteBaseCalculo.class})
+				TesteBaseCalculo.class,
+				TesteImpostoPorFaixa.class})
+
 public class AllTests {
 
 }
